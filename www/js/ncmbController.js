05fb24722f9a94ae25bfd87028caf81622f2b238
k18rs104 <k18rs104@st.kyusan-u.ac.jp>
2019-11-25 13:58:59 +0900

    ncmb内の変更

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -3,4 +3,17 @@
 // ncmbController.js
 
 var ncmbController = {
+  APPLICATION_KEY: appKey,
+    CLIENT_KEY: clientKey,
+
+    ncmb: null,
+    currentUser: null,  // ログインしたユーザーのオブジェクトを格納
+    screenSize: null,    // 画面サイズを格納
+
+    // 初期化
+    init: function(screenSize) {
+        var self = this;
+        self.ncmb = new NCMB(self.APPLICATION_KEY, self.CLIENT_KEY);    // mobile backendの初期化
+        self.screenSize = screenSize;
+    }
 }
